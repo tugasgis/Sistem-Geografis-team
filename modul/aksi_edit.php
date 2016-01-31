08dacd5257a52b13222a112357630b10779f0550
karang24 <tunggal.jaka@gmail.com>
2016-02-01 02:11:54 +0700

    jaka19

--- modul/aksi_edit.php
+++ modul/aksi_edit.php
@@ -9,6 +9,15 @@ include "koneksi.php";
 						   echo"Terimakasih, Data Telah Diubah.";
 						  
   }
-
+  // Apabila password diubah
+  else{
+    $pass=md5($_POST[password]);
+    mysql_query("UPDATE users SET password        = '$pass',
+                                 nama_lengkap    = '$_POST[nama_lengkap]',
+                                 email           = '$_POST[email]',  
+                                 blokir          = '$_POST[blokir]',  
+                                 no_telp         = '$_POST[no_telp]'  
+                           WHERE id_session      = '$_POST[id]'");
+						   echo"Terimakasih, Data Telah Diubah.";
   }
 ?>
