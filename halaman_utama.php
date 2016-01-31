d353b5cfc970c430cc73ee4f67f81df83da3db95
mfauzann <fauzan.jumalianto@gmail.com>
2016-01-31 17:27:04 +0700

    ojan lagi-lagi

--- halaman_utama.php
+++ halaman_utama.php
@@ -49,5 +49,48 @@
 	color: #000;
 }
 -->
- </body>
+</style>
+</head>
+<body>
+	<div id="bawah">Copyright &copy; 2011 by &laquo;<span class="lida"> Firmansyah Wahyudiarto & Lida Pratiwi Puteri</span> &raquo; <br>
+ <span class="telkom">PT. Telkom Indonesia</span> Divisi Consumer Service</div>
+	<div id="wrapper" class="container_16">
+		<div id="top_nav" class="nav_down bar_nav grid_16 round_all">
+<a href="#" class="minimize round_bottom"><span>minimize</span></a><img src="images/header-jadi.jpg" width="936" height="256"></div>
+			
+		<div id="side_nav" class="side_nav grid_3 push_down">
+		  <ul class="clearfix">
+				<li><a class="round_left" href="./halaman_utama.php">
+					<img src="images/icons/grey/admin_user.png">
+					<span>Dashboard</span></a>
+				</li> 
+				<li><a class="round_left" href="#">
+					<img src="images/icons/grey/settings_2.png"><span>Utilities</span>
+					<span class="icon">&nbsp;</span>
+					</a>
+					<ul>
+						<li><a href="#"><img src="images/icons/grey/sign_post.png">Import Data</a>
+                        <span class="icon">&nbsp;</span>
+                        	<ul>
+                        		<li><a href="./modul/form_import_perum.php" target="_blank"><img src="images/icons/grey/sport_shirt.png">Import Perumahan</a></li>
+								<li><a href="./modul/form_import_rk.php" target="_blank"><img src="images/icons/grey/trashcan_2.png">Import RK</a></li>
+                        		<li><a href="./modul/form_import_dp.php" target="_blank"><img src="images/icons/grey/winner_podium.png">Import DP</a></li>
+                        	</ul>
+                        </li>
+                        <li><a href="#"><img src="images/icons/grey/refresh_2.png">Excel Report</a>
+                        <span class="icon">&nbsp;</span>
+                        <ul>
+                        		<li><a href="./modul/report_perum.php"><img src="images/icons/grey/sport_shirt.png">Report Perumahan</a></li>
+								<li><a href="./modul/report_rk.php"><img src="images/icons/grey/trashcan_2.png">Report RK</a></li>
+                        		<li><a href="./modul/report_dp.php"><img src="images/icons/grey/winner_podium.png">Report DP</a></li>
+                                <li><a href="./modul/report_sto.php"><img src="images/icons/grey/winner_podium.png">Report STO</a></li>
+                        	</ul>
+                        </li>					
+					</ul>
+				</li>				
+				<li><a href="./modul/isi_gmaps.php" target="_blank">
+					<img src="images/icons/grey/battery.png">
+					<span>View Kecamatan</span>																	
+	</li>	
+               
 </html>
