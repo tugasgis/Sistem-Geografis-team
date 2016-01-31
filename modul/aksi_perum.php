b54a52bc6fbb25aafbf44f615cdb1771ef105f0c
karang24 <tunggal.jaka@gmail.com>
2016-02-01 02:14:42 +0700

    jaka21

--- modul/aksi_perum.php
+++ modul/aksi_perum.php
@@ -11,6 +11,16 @@ if ($jenis=='perum' AND $act=='hapus'){
   header('location:editperum.php');
 }
 
-
+// Update Perum
+elseif ($jenis=='perum' AND $act=='edit_perum'){
+  mysql_query("UPDATE perumahan SET nama_perum 	   = '$_POST[nama_perum]',
+                                   alamat          = '$_POST[alamat]', 
+                                   daerah  	   	   = '$_POST[daerah]', 
+                                   jumlah_dibangun = '$_POST[jumlah_dibangun]',
+								   sudah_dibangun  = '$_POST[sudah_dibangun]', 
+                                   lat    		   = '$_POST[lat]', 
+                                   lng   		   = '$_POST[lng]'
+								   WHERE id_perumahan  = '$_POST[id]'");
+	header('location:editperum.php');
 }
 ?>
