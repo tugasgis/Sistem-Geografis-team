f1f1a0908a163ff5ce4457687cf1131d5223d872
karang24 <tunggal.jaka@gmail.com>
2016-02-01 02:54:20 +0700

    jaka27

--- modul/aksi_sto.php
+++ modul/aksi_sto.php
@@ -10,5 +10,16 @@ if ($jenis=='sto' AND $act=='hapus'){
   header('location:editsto.php');
 }
 
-
+// Update sto
+elseif ($jenis=='sto' AND $act=='edit_sto'){
+  mysql_query("UPDATE sto SET kode_sto = '$_POST[kode_sto]',
+								   nama_sto        = '$_POST[nama_sto]', 
+                                   alamat          = '$_POST[alamat]', 
+                                   id  	   		   = '$_POST[id]', 
+                                   daerah          = '$_POST[daerah]',
+								   lat    		   = '$_POST[lat]', 
+                                   lng   		   = '$_POST[lng]'
+								   WHERE id_sto     = '$_POST[id]'");
+	header('location:editsto.php');
+}
 ?>
