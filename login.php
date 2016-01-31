ec480cbdcc9117810c1ce9938d87957f4bd1a49e
karang24 <tunggal.jaka@gmail.com>
2016-02-01 00:57:23 +0700

    jaka3

--- login.php
+++ login.php
@@ -4,5 +4,19 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Tugas besar GIS</title>
 <script language="javascript">
-
+function validasi(form){
+  if (form.username.value == ""){
+    alert("Anda belum mengisikan Username.");
+    form.username.focus();
+    return (false);
+  }
+     
+  if (form.password.value == ""){
+    alert("Anda belum mengisikan Password.");
+    form.password.focus();
+    return (false);
+  }
+  return (true);
+}
+</script>
 </html>
