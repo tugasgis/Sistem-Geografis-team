c5820939d0dd4e7deefa1f656ec31ac1b965edd9
karang24 <tunggal.jaka@gmail.com>
2016-02-01 02:11:03 +0700

    jaka18

---
+++ modul/aksi_edit.php
@@ -0,0 +1,14 @@
+<?php
+include "koneksi.php";
+ if (empty($_POST[password])) {
+    mysql_query("UPDATE users SET nama_lengkap   = '$_POST[nama_lengkap]',
+                                  email          = '$_POST[email]',
+                                  blokir         = '$_POST[blokir]',  
+                                  no_telp        = '$_POST[no_telp]'  
+                           WHERE  id_session     = '$_POST[id]'");
+						   echo"Terimakasih, Data Telah Diubah.";
+						  
+  }
+
+  }
+?>
