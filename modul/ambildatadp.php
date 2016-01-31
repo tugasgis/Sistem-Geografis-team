aba3b4b970c8fb8e0e92a750f290419061fefb06
karang24 <tunggal.jaka@gmail.com>
2016-02-01 03:00:09 +0700

    jaka32

--- modul/ambildatadp.php
+++ modul/ambildatadp.php
@@ -1,1 +1,9 @@
-
+<?php
+include "koneksi.php";
+$akhir = $_GET['akhir'];
+if($akhir==1){
+    $query = "SELECT * FROM dp ORDER BY id_dp DESC LIMIT 1";
+}else{
+    $query = "SELECT * FROM dp";
+}
+?>
