646ca00ef8c343306a9c0af9651cc36f582ec82d
karang24 <tunggal.jaka@gmail.com>
2016-02-01 03:03:18 +0700

    jaka33

--- modul/ambildatadp.php
+++ modul/ambildatadp.php
@@ -10,4 +10,16 @@ $data = mysql_query($query);
 
 $json = '{"wilayah": {';
 $json .= '"petak":[ ';
+while($x = mysql_fetch_array($data)){
+    $json .= '{';
+    $json .= '"id_dp":"'.$x['id_dp'].'",
+		"nama":"'.htmlspecialchars($x['kode_dp']).'",
+        "alamat":"'.htmlspecialchars($x['alamat']).'",
+		"kapasitas":"'.htmlspecialchars($x['kapasitas']).'",
+
+		"idrk":"'.$x['id_rk'].'",
+		"x":"'.$x['lat'].'",
+        "y":"'.$x['lng'].'"
+    },';
+}
 ?>
