d66c7e9f0c768de7cc92a995b1bd411c9e68fd01
lugasakta <lugasakta95@gmail.com>
2016-02-01 19:18:38 +0700

    lugas58

--- modul/edit_dp.php
+++ modul/edit_dp.php
@@ -101,3 +101,17 @@ echo "<tr><td colspan=2><input type=submit value=Update>
 <a href="../halaman_utama.php">Kembali</a>
     </div>
   </div>
+<div id="footer">
+		<div class="footer_links">
+                 <a href="#" title="">Copyright &copy; 2011 by Firmansyah Wahyudiarto & Lida Pratiwi Puteri</a><a href="#" title=""></a>       
+        </div>
+    	<div class="copyright"></div>
+</div>
+
+
+
+	
+</div>
+
+</body>
+</html>
