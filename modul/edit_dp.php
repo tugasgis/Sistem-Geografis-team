396d2279b4c7a41b03204e49ed32459148815073
lugasakta <lugasakta95@gmail.com>
2016-02-01 19:04:17 +0700

    lugas43

--- modul/edit_dp.php
+++ modul/edit_dp.php
@@ -4,3 +4,6 @@
 include("class_paging.php");
 ?>
 <title>Telkom Consumer Service Division - Edit Distribution Point</title>
+<link rel="stylesheet" type="text/css" href="style_edit2.css" />
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="jquery-1.4.3.min.js"></script>
