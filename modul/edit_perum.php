3099df80e8bf07f5bb677c6fb8d2ba9217a8935a
lugasakta <lugasakta95@gmail.com>
2016-02-01 14:46:50 +0700

    lugas17

--- modul/edit_perum.php
+++ modul/edit_perum.php
@@ -1,1 +1,7 @@
-
+<?php session_start();
+$nama=$_SESSION['teksnama'];
+$alamat=$_SESSION['teksalamat'];
+$kec=$_SESSION['tekskec'];  
+$jumlah=$_SESSION['teksjumlah']; 
+$sudah=$_SESSION['tekssudah'];  
+?>
