411454ddca6afc4f460fd382e4921d5982fa491e
karang24 <tunggal.jaka@gmail.com>
2016-02-01 14:30:12 +0700

    Update cari_kec.php

--- modul/cari_kec.php
+++ modul/cari_kec.php
@@ -5,5 +5,6 @@ include("connect.php");?>
   <title>Kelompok TB GIS</title>
   <body>
 <p><a href="view_perum_barat.php" target="_blank">Kecamatan Bogor Barat</a></p>
+<p><a href="view_perum_slt.php" target="_blank">Kecamatan Bogor Selatan</a>  </p>
   </head>
   </html>
