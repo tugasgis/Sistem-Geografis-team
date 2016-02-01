800ee3c357c4f53adb9bd6d46e0fa4775b699958
Muhammad Fauzan Jumalianto <fauzan.jumalianto@gmail.com>
2016-02-01 14:44:14 +0700

    fauzan13

--- modul/fitur.php
+++ modul/fitur.php
@@ -1,4 +1,7 @@
 <html>
 <head>
 <?php include("koneksi.php");?>
-<title>Telkom Consumer Service Division - Fitur</title>
+<title>indiHOME Consumer Service Division - Fitur</title>
+<link rel="stylesheet" type="text/css" href="style_fitur.css" />
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="jquery-1.4.3.min.js"></script>
