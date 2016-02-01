ff3d070f178698b1212eaaab0cae06bfdec7ac43
lugasakta <lugasakta95@gmail.com>
2016-02-01 19:04:39 +0700

    lugas44

--- modul/edit_dp.php
+++ modul/edit_dp.php
@@ -7,3 +7,6 @@ include("class_paging.php");
 <link rel="stylesheet" type="text/css" href="style_edit2.css" />
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="jquery-1.4.3.min.js"></script>
+
+<div id="main_content">
+		<div id="top_banner"></div>
