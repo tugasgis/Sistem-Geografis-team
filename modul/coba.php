99c9cafe80e3e183e2da853ee507f7cbe9d0cb2e
lugasakta <lugasakta95@gmail.com>
2016-02-01 17:42:52 +0700

    lugas34

--- modul/coba.php
+++ modul/coba.php
@@ -6,3 +6,15 @@ include("koneksi.php");?>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="../dss/jquery-1.4.3.min.js"></script>
 <script type="text/javascript">
+var peta;
+var pertama = 0;
+var jenis = "rk";
+var namarkx = new Array();
+var alamatx = new Array();
+var kecx = new Array();
+var daerahx = new Array();
+var jumlahx = new Array();
+var sudahx = new Array();
+var i;
+var url;
+var gambar_tanda;
