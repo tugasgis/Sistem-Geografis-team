aaad18230afa6679702f1eafa88d91f98f27ff75
lugasakta <lugasakta95@gmail.com>
2016-02-01 14:47:30 +0700

    lugas18

--- modul/edit_perum.php
+++ modul/edit_perum.php
@@ -3,5 +3,11 @@ $nama=$_SESSION['teksnama'];
 $alamat=$_SESSION['teksalamat'];
 $kec=$_SESSION['tekskec'];  
 $jumlah=$_SESSION['teksjumlah']; 
-$sudah=$_SESSION['tekssudah'];  
+$sudah=$_SESSION['tekssudah'];
+
+<html>
+<title>Firmansyah Wahyudiarto & Lida Pratiwi Puteri - GIS</title>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="../dss/jquery-1.4.3.min.js"></script>
+<script type="text/javascript">
 ?>
