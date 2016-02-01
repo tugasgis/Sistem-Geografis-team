831982782ff81116483d692401183ca9f78f0547
Muhammad Fauzan Jumalianto <fauzan.jumalianto@gmail.com>
2016-02-01 14:44:52 +0700

    fauzan13

--- modul/fitur.php
+++ modul/fitur.php
@@ -5,3 +5,5 @@
 <link rel="stylesheet" type="text/css" href="style_fitur.css" />
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="jquery-1.4.3.min.js"></script>
+<div id="main_content">
+		<div id="top_banner"></div>
