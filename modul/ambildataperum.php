df98de37881dde0a6a060871edf26d9c7159a0d1
Muhammad Fauzan Jumalianto <fauzan.jumalianto@gmail.com>
2016-02-01 14:22:56 +0700

    fauzan7

--- modul/ambildataperum.php
+++ modul/ambildataperum.php
@@ -13,4 +13,6 @@ $json .= '"petak":[ ';
 while($x = mysql_fetch_array($data)){
     $json .= '{';
     $json .= '"id_sto":"'.$x['id_sto'].'",
+    "nama":"'.htmlspecialchars($x['nama_rk']).'",
+    "alamat":"'.htmlspecialchars($x['alamat']).'",
 ?>
