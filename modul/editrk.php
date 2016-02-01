37e86626a5456f93ceb4a098af95875f32f99ecb
lugasakta <lugasakta95@gmail.com>
2016-02-01 20:19:27 +0700

    lugas60

--- modul/editrk.php
+++ modul/editrk.php
@@ -7,3 +7,14 @@ include("class_paging.php");
 <link rel="stylesheet" type="text/css" href="style_edit2.css" />
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="jquery-1.4.3.min.js"></script>
+<div id="main_content">
+		<div id="top_banner"></div>
+        
+  <div id="page_content">
+                <div>
+                    <ul class="menu">
+                    </ul>
+                </div>
+          <div class="clear">
+          	<br>
+          </div>
