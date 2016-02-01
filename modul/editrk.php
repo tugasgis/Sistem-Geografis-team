4836f610b2ab4b912d3831cbf97679abffd98bb8
lugasakta <lugasakta95@gmail.com>
2016-02-01 20:19:00 +0700

    lugas59

--- modul/editrk.php
+++ modul/editrk.php
@@ -3,3 +3,7 @@
 <?php include("koneksi.php");
 include("class_paging.php");
 ?>
+<title>Telkom Consumer Service Division - Edit Rumah Kabel</title>
+<link rel="stylesheet" type="text/css" href="style_edit2.css" />
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="jquery-1.4.3.min.js"></script>
