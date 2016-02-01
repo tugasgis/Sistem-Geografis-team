dd1711dd3483c48822445abcc5e63096f83e21ed
lugasakta <lugasakta95@gmail.com>
2016-02-01 14:50:53 +0700

    lugas19

--- modul/edit_perum.php
+++ modul/edit_perum.php
@@ -10,4 +10,17 @@ $sudah=$_SESSION['tekssudah'];
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="../dss/jquery-1.4.3.min.js"></script>
 <script type="text/javascript">
+//inisialisasi variabel tampung
+var peta;
+var pertama = 0;
+var jenis = "home";
+var namaperumx = new Array();
+var alamatx = new Array();
+var kecx = new Array();
+var daerahx = new Array();
+var jumlahx = new Array();
+var sudahx = new Array();
+var i;
+var url;
+var gambar_tanda;
 ?>
