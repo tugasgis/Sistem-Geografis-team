a01d13efb3dba6c7defc20756148b9a0b07c3fe6
lugasakta <lugasakta95@gmail.com>
2016-02-01 17:40:55 +0700

    lugas32

--- coba.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-include("koneksi.php");?>
-
-<head>
-<title>Firmansyah Wahyudiarto & Lida Pratiwi Puteri - GIS</title>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script type="text/javascript" src="../dss/jquery-1.4.3.min.js"></script>
-<script type="text/javascript">
