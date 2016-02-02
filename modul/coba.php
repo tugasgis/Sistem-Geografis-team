639ce21ec24eec0bdf589c0a625dc7890df4097c
karang24 <tunggal.jaka@gmail.com>
2016-02-02 15:17:49 +0700

    jaka222

--- modul/coba.php
+++ modul/coba.php
@@ -2,7 +2,7 @@
 include("koneksi.php");?>
 
 <head>
-<title>Firmansyah Wahyudiarto & Lida Pratiwi Puteri - GIS</title>
+<title>pelangan indihome</title>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript" src="../dss/jquery-1.4.3.min.js"></script>
 <script type="text/javascript">
